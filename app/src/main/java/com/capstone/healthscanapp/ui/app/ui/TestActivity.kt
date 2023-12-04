8517ccf1c71d69604f21d81e92e99d4706ac7392
ajihermansya <ajiheransya14@gmail.com>
2023-12-04 20:59:20 +0700

    test-repo

---
+++ app/src/main/java/com/capstone/healthscanapp/ui/app/ui/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.healthscanapp.ui.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.healthscanapp.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
