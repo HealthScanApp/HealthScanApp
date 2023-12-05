07730453fb5f9160bc8a14c3aba471b127d27436
HealthScanApp <151711281+HealthScanApp@users.noreply.github.com>
2023-12-05 13:32:08 +0700

    Login dan register tinggal Back end penyambungan ke FIREBASE, Logo sudah di masukkan

--- app/src/main/java/com/capstone/healthscanapp/ui/app/login/LoginActivity.kt
+++ app/src/main/java/com/capstone/healthscanapp/ui/app/login/LoginActivity.kt
@@ -3,57 +3,52 @@ package com.capstone.healthscanapp.ui.app.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import com.capstone.healthscanapp.R
-import com.google.firebase.Firebase
+import com.capstone.healthscanapp.databinding.ActivityLoginBinding
+import com.capstone.healthscanapp.ui.app.home.home_main.HomeActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
-
-    private lateinit var emailEditText: EditText
-    private lateinit var passwordEditText: EditText
-    private lateinit var logInButton: Button
-    private lateinit var googleRegisterButton: Button
-    private lateinit var registerPromptTextView: TextView
-    private lateinit var forgotPasswordTextView: TextView
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        auth = Firebase.auth
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        emailEditText = findViewById(R.id.emailEditText)
-        passwordEditText = findViewById(R.id.passwordEditText)
-        logInButton = findViewById(R.id.logInButton)
-        googleRegisterButton = findViewById(R.id.googleRegisterButton)
-        registerPromptTextView = findViewById(R.id.registerPromptTextView)
-        forgotPasswordTextView = findViewById(R.id.forgotPasswordTextView)
+        firebaseAuth = FirebaseAuth.getInstance()
 
-        // aksi untuk tombol login
-        logInButton.setOnClickListener {
-
-        }
-
-        // aksi untuk tombol Google
-        googleRegisterButton.setOnClickListener {
-
-        }
-
-        // aksi untuk teks "Don't have an account? Sign Up"
-        registerPromptTextView.setOnClickListener {
+        binding.registerPromptTextView.setOnClickListener {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
 
-        // aksi untuk teks "Forgot Password?"
-        forgotPasswordTextView.setOnClickListener {
+        binding.logInButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
 
+            if (email.isEmpty() || password.isEmpty()) {
+                binding.emailEditText.error = "Please fill all the fields"
+                binding.passwordEditText.error = "Please fill all the fields"
+            } else {
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val user = firebaseAuth.currentUser
+                            if (user != null) {
+                                val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                                startActivity(intent)
+                                finish()
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            binding.emailEditText.error = "Invalid email or password"
+                            binding.passwordEditText.error = "Invalid email or password"
+                        }
+                    }
+            }
         }
     }
 }
--- app/src/main/java/com/capstone/healthscanapp/ui/app/login/RegisterActivity.kt
+++ app/src/main/java/com/capstone/healthscanapp/ui/app/login/RegisterActivity.kt
@@ -1,20 +1,66 @@
 package com.capstone.healthscanapp.ui.app.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.capstone.healthscanapp.R
-import com.google.firebase.Firebase
+import android.widget.Toast
+import com.capstone.healthscanapp.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityRegisterBinding
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
 
-        auth = Firebase.auth
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.alreadyHaveAccountTextView.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.RegisterButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                binding.emailEditText.error = "Please fill all the fields"
+                binding.passwordEditText.error = "Please fill all the fields"
+            } else if (password.length < 6) {
+                binding.passwordEditText.error = "Password does not match"
+            } else {
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            val user = firebaseAuth.currentUser
+                            user?.sendEmailVerification()
+                                ?.addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                                        startActivity(intent)
+                                        finish()
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(baseContext, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show()
+                                    }
+                                }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
     }
 }
