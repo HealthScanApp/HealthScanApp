6a810eb1665cf50d37586c68d82d22c3ce8cef22
HealthScanApp <151711281+HealthScanApp@users.noreply.github.com>
2023-12-16 00:00:49 +0700

    Login dan Register Done

--- app/src/main/java/com/capstone/healthscanapp/ui/app/ui/home_menu/TokoBergiziActivity.kt
+++ app/src/main/java/com/capstone/healthscanapp/ui/app/ui/home_menu/TokoBergiziActivity.kt
@@ -1,16 +1,21 @@
 package com.capstone.healthscanapp.ui.app.ui.home_menu
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.capstone.healthscanapp.R
+import com.capstone.healthscanapp.data.CartItem
 
 class TokoBergiziActivity : AppCompatActivity() {
 
@@ -26,6 +31,15 @@ class TokoBergiziActivity : AppCompatActivity() {
 
         val adapter = ProductAdapter(products)
         productsRecyclerView.adapter = adapter
+
+        // Setup the cart ImageView click listener
+        val cartImageView: ImageView = findViewById(R.id.cartImageView)
+        cartImageView.setOnClickListener {
+            // Start the CartActivity when the cart icon is clicked
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     private fun createDummyProductList(): List<Product> {
@@ -60,7 +74,8 @@ class TokoBergiziActivity : AppCompatActivity() {
     inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val productImageView: ImageView = itemView.findViewById(R.id.productImageView)
         private val productNameTextView: TextView = itemView.findViewById(R.id.productNameTextView)
-        private val productPriceTextView: TextView = itemView.findViewById(R.id.productPriceTextView)
+        private val productPriceTextView: TextView =
+            itemView.findViewById(R.id.productPriceTextView)
         private val buyButton: Button = itemView.findViewById(R.id.buyButton)
 
         fun bind(product: Product) {
@@ -68,10 +83,38 @@ class TokoBergiziActivity : AppCompatActivity() {
             productNameTextView.text = product.name
             productPriceTextView.text = product.price
 
-            // Set onClickListener for the buy button if needed
             buyButton.setOnClickListener {
-                // Handle the buy button click
+                // Here you add the product to your cart (you'll need to implement this logic)
+                addToCart(product)
+
+                // Then navigate to CartActivity
+                val intent = Intent(this@TokoBergiziActivity, CartActivity::class.java)
+                startActivity(intent)
             }
         }
+
+        // Dummy function to illustrate adding to cart
+        private fun addToCart(product: Product) {
+            val cartItem = CartItem(product.name, product.price)
+            CartManager.addToCart(cartItem)
+
+            // Optionally show a message to the user
+            Toast.makeText(itemView.context, "Added to cart!", Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.botton_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.cart -> {
+                val intent = Intent(this, CartActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
